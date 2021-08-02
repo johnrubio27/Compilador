f1866b1ec02c4683664602fccd61f3dee1f47a34
jarzuza18 <jarzuza107@gmail.com>
2021-08-02 04:24:42 -0500

    Expresiones regulares

--- src/app/models/lex.model.ts
+++ src/app/models/lex.model.ts
@@ -1,33 +1,42 @@
 
 export class Lexer {
     lineasCodigo: string[];
-    reglasExpReg: RegExp[] = []; 
+    reglasExpReg: RegExp[] = [];
     _validaciones: string[] = [];
-    _numeroLineaCodigo: number = 1; 
+    _numeroLineaCodigo: number = 1;
     constructor(lineasCodigo: string[]){
         this.lineasCodigo = lineasCodigo;
         this.reglasExpReg = [
-            /0*55/g,
-            /ab/,
-            /fg/
+            /[IJ]/,
+            /[a-z]/,
+            /[(0-9{1,6})]/,
+            /\x/,
+            /["+"{1,2}]/,
+            /["-"{1,2}]/,
+            /["/"{1,2}]/,
+            /["%"{1,2}]/,
+            /["*"{1,2}]/,
+            /["="]/,
+            /[">"]/,
+            /["<"]/,
+            /["!"]/
         ];
     }
-    
+
 
     recorrer(){
         for (let i = 0; i < this.lineasCodigo.length; i++) {
             let elementoValido = true;
-
             for (let j = 0; j < this.reglasExpReg.length; j++) {
                 if(this.lineasCodigo[i].match(this.reglasExpReg[j])){
                     elementoValido = true;
-                    this._validaciones.push(`${this._numeroLineaCodigo}     ${this.lineasCodigo[i]} ${this.reglasExpReg[j]}`)
+                    this._validaciones.push(`Es valido ${this.lineasCodigo[i]} en la linea ${this._numeroLineaCodigo} porque cumple la expresion regular ${this.reglasExpReg[j]}`)
                     break;
                 }
                 elementoValido = false;
             }
             if(elementoValido == false){
-                this._validaciones.push(`No es valido ${this.lineasCodigo[i]} ${this._numeroLineaCodigo}`)
+                this._validaciones.push(`No es valido ${this.lineasCodigo[i]} en la linea ${this._numeroLineaCodigo} porque no cumple ninguna regla`)
             }
             this._numeroLineaCodigo++;
         }
